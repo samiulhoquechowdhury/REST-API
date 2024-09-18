92f2fb5347f3a5b23674d8533cc1db27282538b4
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-19 00:43:25 +0530

    express setup

--- server.ts
+++ server.ts
@@ -1,12 +1,8 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
-// import connectDB from "./src/config/db";
 
-const startServer = async () => {
-  // Connect database
-  // await connectDB();
-
-  const port = config.port || 3000;
+const startServer = () => {
+  const port = process.env.PORT || 3000;
 
   app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,11 @@
-import express, { NextFunction, Request, Response } from "express";
-// import cors from "cors";
-// import globalErrorHandler from "./middlewares/globalErrorHandler";
-// import userRouter from "./user/userRouter";
-// import bookRouter from "./book/bookRouter";
-import { config } from "./config/config";
+import express from "express";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: config.frontendDomain,
-//   })
-// );
-
-// app.use(express.json());
-
-// Routes
-// Http methods: GET, POST, PUT, PATCH, DELETE
+//routes
+//http methods GETS, POST, PUT, PATCH, DELETE
 app.get("/", (req, res, next) => {
-  res.json({ message: "Welcome to elib apis" });
+  res.json({ message: 'welcome to elib API' });
 });
-
-// app.use("/api/users", userRouter);
-// app.use("/api/books", bookRouter);
-
-// Global error handler
-// app.use(globalErrorHandler);
 
 export default app;
