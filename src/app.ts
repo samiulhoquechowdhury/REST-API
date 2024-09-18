7a92de247371e40198759012bebf330e35c4d6d2
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-19 02:07:45 +0530

    globla error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 //routes
 //http methods GETS, POST, PUT, PATCH, DELETE
 app.get("/", (req, res, next) => {
-  res.json({ message: 'welcome to elib API' });
+  // throw new Error("something is wrong");
+  const error = createHttpError(400, "Something is wrong");
+  throw error;
+  res.json({ message: "welcome to elib API" });
 });
+
+//global error handler
+app.use(globalErrorHandler)
 
 export default app;
