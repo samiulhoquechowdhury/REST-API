52a181edc5d07a5e0dbdd3f0ba511d427d9c4442
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-22 00:17:56 +0530

    cloudinary complete

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,4 @@
 import { config as conf } from "dotenv";
-
 conf();
 
 const _config = {
@@ -7,6 +6,10 @@ const _config = {
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinarySecret: process.env.CLOUDINARY_API_SECRET,
+  // frontendDomain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
